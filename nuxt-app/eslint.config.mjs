2a539a02b834813637a8ad0b6cbfd1f5388beabf
Ghislaine <ghislaine.aybram@gmail.com>
2024-09-19 13:58:36 +0200

    override linter

--- nuxt-app/eslint.config.mjs
+++ nuxt-app/eslint.config.mjs
@@ -4,9 +4,12 @@ import { createConfigForNuxt } from '@nuxt/eslint-config/flat';
 export default createConfigForNuxt({
   // options here
 })
-  .override('nuxt/typescript/setup', {
-    rules: {
-      // ...Override rules, for example:
-      '@typescript-eslint/no-unused-vars': 'off'
+  .override(
+    'nuxt/typescript/setup', // specify the name of the target config, or index
+    {
+      rules: {
+        // ...Override rules, for example:
+        '@typescript-eslint/no-unused-vars': 'off'
+      }
     }
-  })
+  )
