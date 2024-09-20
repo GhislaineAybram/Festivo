bb55ba313e9e654d4750512d0cf56294472c9e90
Ghislaine <ghislaine.aybram@gmail.com>
2024-09-20 14:25:40 +0200

    navbar modification

--- nuxt-app/components/MenubarCompo.vue
+++ nuxt-app/components/MenubarCompo.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
-import Menubar from 'primevue/menubar'
+import { useRouter } from 'vue-router';
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 
 const router = useRouter()
 
@@ -8,116 +8,74 @@ const { loggedIn, user, session, fetch, clear } = useUserSession()
 
 const userId = computed(() => user.value?.id || null)
 
-const items = ref([
-  {
-    label: 'Accueil',
-    icon: 'pi pi-home',
-    command: () => router.push({ path: '/' }),
-  },
-  {
-    label: 'Mes invitations',
-    icon: 'pi pi-ticket',
-    items: [
-      {
-        label: 'Pot de départ Rémi',
-        icon: 'pi pi-chevron-right',
-        command: () => router.push({ path: '/event/1' }),
-      },
-      {
-        label: 'Anniversaire 3 ans Léopold',
-        icon: 'pi pi-chevron-right',
-        command: () => router.push({ path: '/event/2' }),
-      },
-    ],
-  },
-  {
-    label: 'Mes événements',
-    icon: 'pi pi-crown',
-    items: [
-      {
-        label: 'Anniversaire 40 ans',
-        icon: 'pi pi-chevron-right',
-        command: () => router.push({ path: '/eventadmin/1' }),
-      },
-      {
-        label: 'Baby shower',
-        icon: 'pi pi-chevron-right',
-        command: () => router.push({ path: '/eventadmin/2' }),
-      },
-    ],
-  },
-  {
-    label: 'Connexion',
-    icon: 'pi pi-user',
-    items: [
-      {
-        label: 'Mon profil',
-        icon: 'pi pi-user',
-        command: () => router.push({ path: `/profile/${userId.value}` }),
-      },
-      {
-        label: 'Se connecter',
-        icon: 'pi pi-sign-in',
-        command: () => router.push({ path: '/login' }),
-      },
-      {
-        label: 'Se déconnecter',
-        icon: 'pi pi-sign-out',
-        command: () => clear(),
-      },
-    ],
-  },
-])
+const navigation = [
+  { name: 'Accueil', href: '/', current: true },
+  { name: 'Mes invitations', href: 'event/1', current: false },
+  { name: 'Mes événements', href: 'eventadmin/1', current: false },
+  { name: 'Login', href: '/login', current: false },
+]
 </script>
 
 <template>
-  <div
-    id="menuBar"
-    class="card custom-menubar"
-    unstyled
-  >
-    <Menubar
-      :model="items"
-      class="flex justify-between items-center"
-    >
-      <template #item="{ item, props, hasSubmenu, root }">
-        <a
-          v-ripple
-          class="flex items-center"
-          v-bind="props.action"
-        >
-          <span :class="item.icon" />
-          <span class="ml-2">{{ item.label }}</span>
-          <Badge
-            v-if="item.badge"
-            :class="{ 'ml-auto': !root, 'ml-2': root }"
-            :value="item.badge"
-          />
-          <span
-            v-if="item.shortcut"
-            class="ml-auto border border-surface rounded bg-emphasis text-muted-color text-xs p-1"
-          >{{ item.shortcut }}</span>
-          <i
-            v-if="hasSubmenu"
-            :class="['pi pi-angle-down', { 'pi-angle-down ml-2': root, 'pi-angle-right ml-auto': !root }]"
-          />
-        </a>
-      </template>
-      <template #end>
-        <div class="flex items-center gap-2">
-          <img
-            src="../public/img/festify-high-resolution-logo-square.png"
-            alt="logo Festify"
-            class="h-10"
-          >
-          <Avatar
-            image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png"
-            shape="circle"
-          />
+  <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
+    <div id="menuBar" class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="relative flex h-16 items-center justify-between">
+        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <!-- Mobile menu button-->
+          <DisclosureButton class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+            <span class="absolute -inset-0.5" />
+            <span class="sr-only">Open main menu</span>
+            <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+            <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
+          </DisclosureButton>
         </div>
-      </template>
-    </Menubar>
-  </div>
+        <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+          <div class="flex flex-shrink-0 items-center">
+            <img class="h-8 w-auto" src="../public/img/festify-high-resolution-logo-square.png" alt="Logo site Festify" />
+          </div>
+          <div class="hidden sm:ml-6 sm:block">
+            <div class="flex space-x-4">
+              <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+            </div>
+          </div>
+        </div>
+        <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+          <button type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+            <span class="absolute -inset-1.5" />
+            <span class="sr-only">View notifications</span>
+            <BellIcon class="h-6 w-6" aria-hidden="true" />
+          </button>
+
+          <!-- Profile dropdown -->
+          <Menu as="div" class="relative ml-3">
+            <div>
+              <MenuButton class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                <span class="absolute -inset-1.5" />
+                <span class="sr-only">Open user menu</span>
+                <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+              </MenuButton>
+            </div>
+            <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+              <MenuItems class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <MenuItem v-slot="{ active }">
+                  <a href="/profile/1" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">My profile</a>
+                </MenuItem>
+                <MenuItem v-slot="{ active }">
+                  <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Sign out</a>
+                </MenuItem>
+              </MenuItems>
+            </transition>
+          </Menu>
+        </div>
+      </div>
+    </div>
+
+    <DisclosurePanel class="sm:hidden">
+      <div class="space-y-1 px-2 pb-3 pt-2">
+        <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+      </div>
+    </DisclosurePanel>
+  </Disclosure>
 </template>
 
 <style  lang="scss" scoped>
