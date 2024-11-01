4cb41d6e983a809e2df9c46155808b8e18a39efc
Ghislaine <ghislaine.aybram@gmail.com>
2024-11-01 10:48:30 +0100

    refacto index page

---
+++ nuxt-app/components/CelebrationCardPast.vue
@@ -0,0 +1,188 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import type { Celebration } from '~/types'
+
+export function getMonth(dateString: string) {
+  const date = new Date(dateString)
+  return date.toLocaleString('default', { month: 'long' })
+}
+export function getDay(dateString: string) {
+  const date = new Date(dateString)
+  return date.getDate()
+}
+
+export default defineComponent({
+  name: 'CelebrationsList',
+  props: {
+    celebrations: {
+      type: Array as PropType<Celebration[]>,
+      required: true,
+    },
+  },
+  setup(props) {
+    const formattedCelebrations = computed(() =>
+      props.celebrations.map(celebration => ({
+        ...celebration,
+        dateMonth: getMonth(celebration.date),
+        dateDay: getDay(celebration.date),
+      })),
+    )
+
+    return { formattedCelebrations }
+  },
+})
+</script>
+
+<template>
+  <div
+    v-for="celebration in formattedCelebrations"
+    :key="celebration.celebration_id"
+    class="parent"
+  >
+    <div class="card">
+      <div class="finished">
+        FINISHED
+      </div>
+      <div class="card-image" />
+      <div class="category">
+        {{ celebration.name }}
+      </div>
+      <div class="date-box">
+        <span class="date">{{ celebration.dateDay }}</span>
+        <span class="month">{{ celebration.dateMonth }}</span>
+      </div>
+      <div class="heading">
+        {{ celebration.description }}
+      </div>
+      <a
+        class="action"
+        :href="`/celebration/${celebration.celebration_id}`"
+      >
+        {{ $t("welcome.event_link") }}
+        <span aria-hidden="true">→</span>
+      </a>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.parent {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  justify-content: center;
+  padding: 35px;
+  opacity: 0.5;
+}
+
+.card {
+  position: relative;
+  flex: 1 1 calc(100% - 2rem);
+  max-width: 260px;
+  background: white;
+  padding: 0.5em;
+  border-radius: 6px;
+  border: 1px solid $indigo;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 20px -10px;
+}
+
+.card-image {
+  background-color: rgb(236, 236, 236);
+  width: 100%;
+  height: 130px;
+  border-radius: 6px 6px 0 0;
+}
+
+.card-image:hover {
+  transform: scale(0.98);
+}
+
+.category {
+  text-transform: uppercase;
+  font-size: 0.8em;
+  font-weight: 600;
+  color: $indigo;
+  padding: 10px 7px 0;
+  background-color: white;
+}
+
+.category:hover {
+  cursor: pointer;
+}
+
+.heading {
+  font-weight: 600;
+  color: $grey;
+  padding: 7px;
+  background-color: white;
+}
+
+.heading:hover {
+  cursor: pointer;
+}
+
+.date-box {
+  position: absolute;
+  top: -20px;
+  right: -20px;
+  height: 60px;
+  width: 60px;
+  background: white;
+  border: 1px solid $tangerine;
+  border-radius: 10px;
+  padding: 5px;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 17px 10px -10px;
+}
+
+.date-box span {
+  display: block;
+  text-align: center;
+  background-color: white;
+}
+
+.date-box .month {
+  color: $indigo;
+  font-size: 11px;
+  font-weight: 700;
+}
+
+.date-box .date {
+  font-size: 20px;
+  font-weight: 900;
+  color: $indigo;
+}
+
+.action {
+  display: inline-flex;
+  margin-top: 1rem;
+  color: $indigo;
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  font-weight: 500;
+  align-items: center;
+  gap: 0.25rem;
+  background-color: $whisper;
+  padding: 4px 8px;
+  border-radius: 4px;
+  border: 1px solid $indigo;
+  border-radius: 10px;
+}
+.action span {
+  background-color: $whisper;
+}
+
+.finished {
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: $grey;
+  color: white;
+  font-size: 0.75em;
+  font-weight: bold;
+  padding: 8px 15px;
+  border-top-left-radius: 4px;
+  border-bottom-right-radius: 4px;
+  z-index: 2;
+  text-transform: uppercase;
+}
+</style>
--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { getDay, getMonth } from '~/components/CelebrationCard.vue'
 import type { Celebration } from '~/types'
 
 const { auth } = useSupabaseClient()
@@ -15,75 +16,42 @@ watchEffect(() => {
   }
 })
 
-const getMonth = (dateString: string) => {
-  const date = new Date(dateString)
-  return date.toLocaleString('default', { month: 'long' })
-}
-const getDay = (dateString: string) => {
-  const date = new Date(dateString)
-  return date.getDate()
-}
-
 const runtimeConfig = useRuntimeConfig()
 
-const { data: upcomingCelebrationCreated, error: upcomingCelebrationCreatedError } = await useFetch<Celebration[]>(
-  () => `${runtimeConfig.public.apiUrl}/celebrations/upcoming/author/${user.id}`,
-)
-if (upcomingCelebrationCreatedError.value) {
-  console.error('Failed to fetch celebration data', upcomingCelebrationCreatedError.value)
-}
-const upcomingCelebrationsCreated = computed(
-  () =>
-    upcomingCelebrationCreated.value?.map(celebration => ({
+async function fetchCelebrations(url: string, errorMessage: string) {
+  const { data, error } = await useFetch<Celebration[]>(() => url)
+
+  if (error.value) {
+    console.error(errorMessage, error.value)
+  }
+
+  return computed(() =>
+    data.value?.map(celebration => ({
       ...celebration,
       dateMonth: getMonth(celebration.date),
       dateDay: getDay(celebration.date),
     })) || [],
+  )
+}
+
+const upcomingCelebrationsCreated = await fetchCelebrations(
+  `${runtimeConfig.public.apiUrl}/celebrations/upcoming/author/${user.id}`,
+  'Failed to fetch upcoming created celebrations data',
 )
 
-const { data: upcomingCelebrationInvited, error: upcomingCelebrationInvitedError } = await useFetch<Celebration[]>(
-  () => `${runtimeConfig.public.apiUrl}/celebrations/upcoming/guest/${user.id}`,
-)
-if (upcomingCelebrationInvitedError.value) {
-  console.error('Failed to fetch celebration data', upcomingCelebrationInvitedError.value)
-}
-const upcomingCelebrationsInvited = computed(
-  () =>
-    upcomingCelebrationInvited.value?.map(celebration => ({
-      ...celebration,
-      dateMonth: getMonth(celebration.date),
-      dateDay: getDay(celebration.date),
-    })) || [],
+const upcomingCelebrationsInvited = await fetchCelebrations(
+  `${runtimeConfig.public.apiUrl}/celebrations/upcoming/guest/${user.id}`,
+  'Failed to fetch upcoming celebrations invitations data',
 )
 
-const { data: pastCelebrationCreated, error: pastCelebrationCreatedError } = await useFetch<Celebration[]>(
-  () => `${runtimeConfig.public.apiUrl}/celebrations/past/author/${user.id}`,
-)
-if (pastCelebrationCreatedError.value) {
-  console.error('Failed to fetch celebration data', pastCelebrationCreatedError.value)
-}
-const pastCelebrationsCreated = computed(
-  () =>
-    pastCelebrationCreated.value?.map(celebration => ({
-      ...celebration,
-      dateMonth: getMonth(celebration.date),
-      dateDay: getDay(celebration.date),
-    })) || [],
+const pastCelebrationsCreated = await fetchCelebrations(
+  `${runtimeConfig.public.apiUrl}/celebrations/past/author/${user.id}`,
+  'Failed to fetch past created celebrations data',
 )
 
-const { data: pastCelebrationInvited, error: pastCelebrationInvitedError } = await useFetch<Celebration[]>(
-  () => `${runtimeConfig.public.apiUrl}/celebrations/past/guest/${user.id}`,
-)
-if (pastCelebrationInvitedError.value) {
-  console.error('Failed to fetch celebration data', pastCelebrationInvitedError.value)
-}
-const pastCelebrationsInvited = computed(
-  () =>
-    pastCelebrationInvited.value?.map(celebration => ({
-      ...celebration,
-      dateMonth: getMonth(celebration.date),
-      dateDay: getDay(celebration.date),
-    })) || [],
+const pastCelebrationsInvited = await fetchCelebrations(
+  `${runtimeConfig.public.apiUrl}/celebrations/past/guest/${user.id}`,
+  'Failed to fetch past celebrations invitations data',
 )
 </script>
 
@@ -101,73 +69,10 @@ const pastCelebrationsInvited = computed(
         {{ $t("welcome.events") }}
       </h2>
       <!-- upcoming celebrations created -->
-      <div
-        v-if="upcomingCelebrationsCreated && upcomingCelebrationsCreated.length > 0"
-        class="grid grid-cols-1 sm:grid-cols-1"
-      >
-        <div
-          v-for="celebration in upcomingCelebrationsCreated"
-          :key="celebration.celebration_id"
-          class="parent"
-        >
-          <div class="card">
-            <div class="card-image" />
-            <div class="category">
-              {{ celebration.name }}
-            </div>
-            <div class="date-box">
-              <span class="date">{{ celebration.dateDay }}</span>
-              <span class="month">{{ celebration.dateMonth }}</span>
-            </div>
-            <div class="heading">
-              {{ celebration.description }}
-            </div>
-            <a
-              class="action"
-              :href="`/celebration/${celebration.celebration_id}`"
-            >
-              {{ $t("welcome.event_link") }}
-              <span aria-hidden="true">→</span>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div v-else>
-        <p>{{ $t("celebration.no-celebration-created") }}</p>
-      </div>
+      <CelebrationCard :celebrations="upcomingCelebrationsCreated" />
 
       <!-- past celebrations created -->
-      <div
-        v-if="pastCelebrationsCreated && pastCelebrationsCreated.length > 0"
-        class="grid grid-cols-1 sm:grid-cols-1"
-      >
-        <div
-          v-for="celebration in pastCelebrationsCreated"
-          :key="celebration.celebration_id"
-          class="parent"
-        >
-          <div class="card">
-            <div class="card-image" />
-            <div class="category">
-              {{ celebration.name }} - PAST !!!
-            </div>
-            <div class="date-box">
-              <span class="date">{{ celebration.dateDay }}</span>
-              <span class="month">{{ celebration.dateMonth }}</span>
-            </div>
-            <div class="heading">
-              {{ celebration.description }}
-            </div>
-            <a
-              class="action"
-              :href="`/celebration/${celebration.celebration_id}`"
-            >
-              {{ $t("welcome.event_link") }}
-              <span aria-hidden="true">→</span>
-            </a>
-          </div>
-        </div>
-      </div>
+      <CelebrationCardPast :celebrations="pastCelebrationsCreated" />
 
       <h2
         class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight"
@@ -175,73 +80,10 @@ const pastCelebrationsInvited = computed(
         {{ $t("welcome.invitations") }}
       </h2>
       <!-- upcoming invitations -->
-      <div
-        v-if="upcomingCelebrationsInvited && upcomingCelebrationsInvited.length > 0"
-        class="grid grid-cols-1 sm:grid-cols-1"
-      >
-        <div
-          v-for="celebration in upcomingCelebrationsInvited"
-          :key="celebration.celebration_id"
-          class="parent"
-        >
-          <div class="card">
-            <div class="card-image" />
-            <div class="category">
-              {{ celebration.name }}
-            </div>
-            <div class="date-box">
-              <span class="date">{{ celebration.dateDay }}</span>
-              <span class="month">{{ celebration.dateMonth }}</span>
-            </div>
-            <div class="heading">
-              {{ celebration.description }}
-            </div>
-            <a
-              class="action"
-              :href="`/celebration/${celebration.celebration_id}`"
-            >
-              {{ $t("welcome.event_link") }}
-              <span aria-hidden="true">→</span>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div v-else>
-        <p>{{ $t("celebration.no-celebration-invited") }}</p>
-      </div>
+      <CelebrationCard :celebrations="upcomingCelebrationsInvited" />
 
       <!-- past invitations -->
-      <div
-        v-if="pastCelebrationsInvited && pastCelebrationsInvited.length > 0"
-        class="grid grid-cols-1 sm:grid-cols-1"
-      >
-        <div
-          v-for="celebration in pastCelebrationsInvited"
-          :key="celebration.celebration_id"
-          class="parent"
-        >
-          <div class="card">
-            <div class="card-image" />
-            <div class="category">
-              {{ celebration.name }} - PAST !!!
-            </div>
-            <div class="date-box">
-              <span class="date">{{ celebration.dateDay }}</span>
-              <span class="month">{{ celebration.dateMonth }}</span>
-            </div>
-            <div class="heading">
-              {{ celebration.description }}
-            </div>
-            <a
-              class="action"
-              :href="`/celebration/${celebration.celebration_id}`"
-            >
-              {{ $t("welcome.event_link") }}
-              <span aria-hidden="true">→</span>
-            </a>
-          </div>
-        </div>
-      </div>
+      <CelebrationCardPast :celebrations="pastCelebrationsInvited" />
     </div>
 
     <div v-else>
@@ -276,107 +118,7 @@ h2 {
   align-content: center;
   text-align: center;
 }
-.parent {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 1rem;
-  justify-content: center;
-  padding: 35px;
-}
 
-.card {
-  position: relative;
-  flex: 1 1 calc(100% - 2rem);
-  max-width: 260px;
-  background: white;
-  padding: 0.5em;
-  border-radius: 6px;
-  border: 1px solid $indigo;
-  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 20px -10px;
-}
-
-.card-image {
-  background-color: rgb(236, 236, 236);
-  width: 100%;
-  height: 130px;
-  border-radius: 6px 6px 0 0;
-}
-
-.card-image:hover {
-  transform: scale(0.98);
-}
-
-.category {
-  text-transform: uppercase;
-  font-size: 0.8em;
-  font-weight: 600;
-  color: $indigo;
-  padding: 10px 7px 0;
-  background-color: white;
-}
-
-.category:hover {
-  cursor: pointer;
-}
-
-.heading {
-  font-weight: 600;
-  color: $grey;
-  padding: 7px;
-  background-color: white;
-}
-
-.heading:hover {
-  cursor: pointer;
-}
-
-.date-box {
-  position: absolute;
-  top: -20px;
-  right: -20px;
-  height: 60px;
-  width: 60px;
-  background: white;
-  border: 1px solid $tangerine;
-  border-radius: 10px;
-  padding: 5px;
-  box-shadow: rgba(100, 100, 111, 0.2) 0px 17px 10px -10px;
-}
-
-.date-box span {
-  display: block;
-  text-align: center;
-  background-color: white;
-}
-
-.date-box .month {
-  color: $indigo;
-  font-size: 11px;
-  font-weight: 700;
-}
-
-.date-box .date {
-  font-size: 20px;
-  font-weight: 900;
-  color: $indigo;
-}
-
-.action {
-  display: inline-flex;
-  margin-top: 1rem;
-  color: $indigo;
-  font-size: 0.875rem;
-  line-height: 1.25rem;
-  font-weight: 500;
-  align-items: center;
-  gap: 0.25rem;
-  background-color: $tangerine;
-  padding: 4px 8px;
-  border-radius: 4px;
-}
-.action span {
-  background-color: $tangerine;
-}
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
